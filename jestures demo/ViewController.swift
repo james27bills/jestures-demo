dfda49c5853a54d3f91d55ba5c48a44a4407612a
JAMES BILLS <rl236693@rlas-macair-26.local>
2018-11-08 11:16:22 -0600

    Initial Commit

---
+++ jestures demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  jestures demo
+//
+//  Created by JAMES BILLS on 11/8/18.
+//  Copyright © 2018 JAMES BILLS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
